5e12bfabbb1508eebae5172b1f98db930da33aa4
kepha <kepha@DESKTOP-830OF6F>
2017-01-16 11:28:10 -0600

    Add project files.

---
+++ ChristmasTree/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var HEIGHT = 7;
+            var maxSpaces = HEIGHT;
+
+            for (var i = 0; i <= HEIGHT; i++, maxSpaces--)
+            {
+                if (i < HEIGHT)
+                {
+                    // print spaces
+                    for (var j = 0; j < maxSpaces; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    // print asterisks
+                    for (var k = 0; k <= i; k++)
+                    {
+                        Console.Write("* ");
+                    }
+                }
+                else
+                {
+                    for (var j = 0; j < HEIGHT; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    // print asterisks
+                    Console.WriteLine("*");
+                }
+                // print more spaces
+                Console.WriteLine();
+                
+
+            }
+        }
+    }
+}
---
+++ ChristmasTree2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChristmasTree2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int HEIGHT = 7;
+
+
+            var lines = new char[HEIGHT, HEIGHT];
+
+            // print vertically
+            for (int i = 1; i <= HEIGHT; i++)
+            {
+                var numOfSpaces = HEIGHT - i;
+
+                // print line
+                for (int i = 0; i < array3D.Rank; i++)
+                {
+                    total *= array3D.GetLength(i);
+                }
+                // print spaces
+                //for (int j = 0; j < maxSpaces; j++)
+                //{
+                //    Console.Write(" ");
+                //}
+                //// print asterisks
+                //for (int k = 0; k <= i; k++)
+                //{
+                //    Console.Write("* ");
+                //}
+                //// print more spaces
+                //Console.WriteLine();
+                //if (i == MAX_HEIGHT)
+                //{
+                //    for (int j = 0; j < MAX_HEIGHT; j++)
+                //    {
+                //        Console.Write(" ");
+                //    }
+                //    // print asterisks
+                //    Console.WriteLine("*");
+
+
+                //}
+            }
+    }
+}
